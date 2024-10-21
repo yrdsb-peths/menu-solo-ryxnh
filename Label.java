cba6ebd2e27bde9f2cea786d372c352bc8b1cc64
ryxnh <349739326@gapps.yrdsb.ca>
2024-10-20 23:36:09 -0400

    deleted author comments / description

--- Label.java
+++ Label.java
@@ -1,16 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 
-/**
- * A Label class that allows you to display a textual value on screen.
- * 
- * The Label is an actor, so you will need to create it, and then add it to the world
- * in Greenfoot.  If you keep a reference to the Label then you can change the text it
- * displays.  
- *
- * @author Amjad Altadmri 
- * @version 1.1
- */
+
 public class Label extends Actor
 {
     private String value;
