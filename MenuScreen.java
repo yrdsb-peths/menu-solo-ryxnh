4089cce190401549672f77ee092b934d5f263330
ryxnh <349739326@gapps.yrdsb.ca>
2024-10-21 00:00:55 -0400

    changed code and added queue and stack from slides

--- MenuScreen.java
+++ MenuScreen.java
@@ -1,24 +1,23 @@
 import greenfoot.*;
-import java.util.Queue;
-import java.util.LinkedList;
 
 public class MenuScreen extends World {
-    private Queue<GreenfootImage> avatars;
+    private Queue<GreenfootImage> avatars; 
     private Avatar avatarActor;
     
     public MenuScreen() {    
         super(600, 400, 1);
     
         //linked list for the images of the avatar
-        avatars = new LinkedList<>();
-        avatars.add(new GreenfootImage("face_a.png"));
-        avatars.add(new GreenfootImage("face_b.png"));
-        avatars.add(new GreenfootImage("face_c.png"));
+         avatars = new Queue<>();
+        avatars.enqueue(new GreenfootImage("face_a.png"));
+        avatars.enqueue(new GreenfootImage("face_b.png"));
+        avatars.enqueue(new GreenfootImage("face_c.png"));
         
         //create avatar object with first image in queue
-        avatarActor = new Avatar (avatars.peek());
-        addObject(avatarActor, getWidth()/ 2, getHeight()/ 2); 
-        
+        if (!avatars.isEmpty()) {
+            avatarActor = new Avatar(avatars.dequeue()); 
+            addObject(avatarActor, getWidth() / 2, getHeight() / 2); 
+        }
         addObject(new Button(this::goHighScore, "High score screen"), 300, 380);
         addObject(new Button(this::goInstructions, "Instructions"), 300, 340);
         addObject(new Button(this::cycleAvatar, "Next Avatar"), 300, 300);
@@ -38,7 +37,7 @@ public class MenuScreen extends World {
     
     // cycle through the avatars in the queue
     public void cycleAvatar() {
-        avatars.add(avatars.remove()); 
-        avatarActor.setImage(avatars.peek());
+        avatars.enqueue(avatarActor.getImage()); //set current avatar in queue
+        avatarActor.setImage(avatars.dequeue()); //set to next avatar
     }
 }
